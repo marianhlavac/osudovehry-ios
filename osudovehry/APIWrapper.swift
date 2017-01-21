eb23736305e8a9eb8e7ec5ba8e0e74bca9883c02
Marián Hlaváč <hlavac95@gmail.com>
2017-01-21 21:58:01 +0100

    Create APIWrapper object

---
+++ osudovehry/APIWrapper.swift
@@ -0,0 +1,44 @@
+//
+//  APIWrapper.swift
+//  osudovehry
+//
+//  Created by Marián Hlaváč on 21/01/2017.
+//  Copyright © 2017 majko. All rights reserved.
+//
+
+import Alamofire
+import SwiftyJSON
+
+class APIWrapper {
+    
+    enum Result {
+        case success(JSON)
+        case failure(Error)
+    }
+    
+    // This is singleton
+    private init() { }
+    static let service: APIWrapper = APIWrapper()
+    
+    var events : [Event] = []
+    
+    func fetchData(completion: @escaping (Result) -> Void) {
+        Alamofire.request("http://private-1f7cd-osudovehry.apiary-mock.com/events").responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                
+                self.events.removeAll()
+                for event in json.arrayValue {
+                    self.events.append(Event(json: event))
+                }
+                
+                NotificationCenter.default.post(name: NotificationTypes.dataChange, object: nil)
+                
+                completion(.success(json))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+}
