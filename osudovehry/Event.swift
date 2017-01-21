69fdcf741d3314265ce9074835bb2176c5e5a2b6
Marián Hlaváč <hlavac95@gmail.com>
2017-01-21 21:41:05 +0100

    Create Event object

--- osudovehry/Event.swift
+++ osudovehry/Event.swift
@@ -6,4 +6,25 @@
 //  Copyright © 2017 majko. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import SwiftyJSON
+
+class Event: NSObject {
+    let name: String
+    let desc: String
+    let startsAt: String
+    let endsAt: String
+    let price: Int
+    let coverPhotoUrl: String
+    let attendeeCount: Int
+    
+    init(json: JSON) {
+        name = json["name"].stringValue
+        desc = json["description"].stringValue
+        startsAt = json["starts-at"].dateValue!
+        endsAt = json["ends-at"].dateValue!
+        price = json["price"].intValue
+        coverPhotoUrl = json["cover-photo-url"].stringValue
+        attendeeCount = json["attendee-count"].intValue
+    }
+}
