ac3ab312515c8e1e16b00c0cfd27ad1ebece980d
Marián Hlaváč <hlavac95@gmail.com>
2017-01-21 21:52:37 +0100

    Create event table view

---
+++ osudovehry/EventTableViewController.swift
@@ -0,0 +1,83 @@
+//
+//  EventTableViewController.swift
+//  osudovehry
+//
+//  Created by Marián Hlaváč on 21/01/2017.
+//  Copyright © 2017 majko. All rights reserved.
+//
+
+import UIKit
+import SwiftyJSON
+
+enum EventTableType {
+    case all
+    case upcoming
+}
+
+class EventTableViewController: UITableViewController {
+    
+    let type: EventTableType
+    var events = [Event]()
+    
+    init(type: EventTableType) {
+        super.init(nibName: nil, bundle: nil)
+        self.type = type
+        
+        // Add data receiving observer
+        NotificationCenter.default.addObserver(self, selector: #selector(self.update), name: NotificationTypes.dataChange, object: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func update() {
+        switch type {
+        case .upcoming:
+            
+        case .all:
+        }
+        
+        tableView.reloadData()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // Set navigation title, depending on table type
+        switch type {
+        case .upcoming:
+            tabBarController?.navigationItem.title = "All Events"
+        case .all:
+            tabBarController?.navigationItem.title = "Upcoming Events"
+        }
+        
+        // Set this class as data source
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return events.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        cell.textLabel?.text = events[indexPath.row].name
+
+        return cell
+    }
+}
---
+++ osudovehry/NotificationTypes.swift
@@ -0,0 +1,13 @@
+//
+//  Notifications.swift
+//  osudovehry
+//
+//  Created by Marián Hlaváč on 21/01/2017.
+//  Copyright © 2017 majko. All rights reserved.
+//
+
+import UIKit
+
+class NotificationTypes {
+    static let dataChange = NSNotification.Name(rawValue: "DATA_CHANGE")
+}
